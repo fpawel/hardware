6ff81de68e8e12a11dba93334cde21e7deded0cd
binf1611@gmail.com <fpawel>
2019-12-16 09:59:46 +0300

    save:wip

--- temp/tempmil82/t2500.go
+++ temp/tempmil82/t2500.go
@@ -6,25 +6,25 @@ import (
 	"github.com/fpawel/comm"
 )
 
-type T2500 struct{ r ResponseReader }
+type T2500 struct{ ResponseReader }
 
 func (x T2500) Start(log comm.Logger, ctx context.Context) error {
-	_, err := getResponseMil82(log, ctx, x.r, "01WRD,01,0102,0001")
+	_, err := getResponseMil82(log, ctx, x.ResponseReader, "01WRD,01,0102,0001")
 	return err
 }
 
 func (x T2500) Stop(log comm.Logger, ctx context.Context) error {
-	_, err := getResponseMil82(log, ctx, x.r, "01WRD,01,0102,0004")
+	_, err := getResponseMil82(log, ctx, x.ResponseReader, "01WRD,01,0102,0004")
 	return err
 }
 
 func (x T2500) Setup(log comm.Logger, ctx context.Context, value float64) error {
 	v := int64(value * 10)
 	s := fmt.Sprintf("01WRD,01,0104,%04X", v)
-	_, err := getResponseMil82(log, ctx, x.r, s)
+	_, err := getResponseMil82(log, ctx, x.ResponseReader, s)
 	return err
 }
 
 func (x T2500) Read(log comm.Logger, ctx context.Context) (float64, error) {
-	return getResponseMil82(log, ctx, x.r, "01RRD,02,0001,0002")
+	return getResponseMil82(log, ctx, x.ResponseReader, "01RRD,02,0001,0002")
 }
--- temp/tempmil82/t800.go
+++ temp/tempmil82/t800.go
@@ -7,26 +7,26 @@ import (
 )
 
 type T800 struct {
-	r ResponseReader
+	ResponseReader
 }
 
 func (x T800) Start(log comm.Logger, ctx context.Context) error {
-	_, err := getResponseMil82(log, ctx, x.r, "01WRD,01,0101,0001")
+	_, err := getResponseMil82(log, ctx, x.ResponseReader, "01WRD,01,0101,0001")
 	return err
 }
 
 func (x T800) Stop(log comm.Logger, ctx context.Context) error {
-	_, err := getResponseMil82(log, ctx, x.r, "01WRD,01,0101,0004")
+	_, err := getResponseMil82(log, ctx, x.ResponseReader, "01WRD,01,0101,0004")
 	return err
 }
 
 func (x T800) Setup(log comm.Logger, ctx context.Context, value float64) error {
 	v := int64(value * 10)
 	s := fmt.Sprintf("01WRD,01,0102,%04X", v)
-	_, err := getResponseMil82(log, ctx, x.r, s)
+	_, err := getResponseMil82(log, ctx, x.ResponseReader, s)
 	return err
 }
 
 func (x T800) Read(log comm.Logger, ctx context.Context) (float64, error) {
-	return getResponseMil82(log, ctx, x.r, "01RRD,02,0001,0002")
+	return getResponseMil82(log, ctx, x.ResponseReader, "01RRD,02,0001,0002")
 }
