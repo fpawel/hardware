d6b2e02d4261c5bf14b2dd31853d5813c4828c92
binf1611@gmail.com <fpawel>
2020-03-11 12:32:04 +0300

    fix error read temperature

--- temp/tempcomport/response.go
+++ temp/tempcomport/response.go
@@ -16,33 +16,33 @@ var Err = merry.New("ошибка термокамеры")
 
 type HandleResponseFunc = func(request, response string)
 
-func getResponse(log comm.Logger, ctx context.Context, what string, cm comm.T, strRequest string) (float64, error) {
+func getResponse(log comm.Logger, ctx context.Context, what string, cm comm.T, strRequest string, temperature *float64) error {
 	log = pkg.LogPrependSuffixKeys(log,
 		"request_temperature_device", strRequest,
 		"temperature_device_command", fmt.Sprintf("`%s`", what),
 	)
-	strRequest = fmt.Sprintf("\x02%s\r\n", strRequest)
-	var temperature float64
-	response, err := cm.GetResponse(log, ctx, []byte(strRequest))
+	strRawRequest := fmt.Sprintf("\x02%s\r\n", strRequest)
 
-	if err == nil {
-		err = checkResponse(strRequest, response, &temperature)
-	}
-
-	if err != nil {
-		err = merry.Prependf(err, "термокамера: %s: request_temperature_device=%q", what, strRequest)
+	wrapErr := func(response []byte, err error) error {
+		err = merry.Prependf(err, "термокамера: %s: request_temperature_device=%q", what, strRawRequest)
 		if len(response) > 0 {
 			err = merry.Prependf(err, "response_temperature_device=%q", string(response))
 		}
-		return 0, merry.WithCause(err, Err)
+		return merry.WithCause(err, Err)
 	}
-
-	return temperature, err
+	if response, err := cm.GetResponse(log, ctx, []byte(strRawRequest)); err != nil {
+		return wrapErr(response, err)
+	} else {
+		if err := checkResponse(response, temperature); err == nil {
+			return wrapErr(response, err)
+		}
+	}
+	return nil
 }
 
 var regexTemperature = regexp.MustCompile(`^01RRD,OK,([0-9a-fA-F]{4}),([0-9a-fA-F]{4})$`)
 
-func checkResponse(strRequest string, response []byte, t *float64) error {
+func checkResponse(response []byte, temperature *float64) error {
 
 	if len(response) == 4 {
 		return merry.New("нет ответа от термокамеры")
@@ -61,29 +61,29 @@ func checkResponse(strRequest string, response []byte, t *float64) error {
 		return merry.New("ответ должен оканчиваться байтами 0D 0A")
 	}
 
+	if temperature == nil {
+		return nil
+	}
+
 	r := strResponse[1 : len(strResponse)-2]
 
-	if strings.HasPrefix(strRequest, "01WRD") && r != "01WRD,OK" {
-		return merry.New("ответ на запрос 01WRD должен быть 01WRD,OK")
+	if r != "01WRD,OK" {
+		return merry.New("не удалось считать температуру: ответ на запрос температуры 01RRD должен начинаться со строки 01RRD,OK")
 	}
 
-	if strings.HasPrefix(strRequest, "01RRD") {
-		if !strings.HasPrefix(r, "01RRD,OK") {
-			return merry.New("не удалось считать температуру: ответ на запрос 01RRD должен начинаться со строки 01RRD,OK")
-		}
-		xs := regexTemperature.FindAllStringSubmatch(r, -1)
-		if len(xs) == 0 {
-			return merry.New("не правильный формат температуры")
-		}
-		if len(xs[1]) == 2 {
-			return merry.New("не правильный формат температуры: ожидался код значения температуры и уставки")
-		}
-		n, err := strconv.ParseInt(xs[1][1], 16, 64)
-		if err != nil {
-			err = errors.Wrapf(err, "не правильный формат температуры: %q", xs[1][1])
-			return merry.Wrap(err)
-		}
-		*t = float64(n) / 10
+	xs := regexTemperature.FindAllStringSubmatch(r, -1)
+	if len(xs) == 0 {
+		return merry.New("не правильный формат температуры")
 	}
+	if len(xs[1]) == 2 {
+		return merry.New("не правильный формат температуры: ожидался код значения температуры и уставки")
+	}
+	n, err := strconv.ParseInt(xs[1][1], 16, 64)
+	if err != nil {
+		err = errors.Wrapf(err, "не правильный формат температуры: %q", xs[1][1])
+		return merry.Wrap(err)
+	}
+	*temperature = float64(n) / 10
+
 	return nil
 }
--- temp/tempcomport/t2500.go
+++ temp/tempcomport/t2500.go
@@ -12,22 +12,20 @@ type T2500 comm.T
 var _ temp.TemperatureDevice = T2500{}
 
 func (x T2500) Start(log comm.Logger, ctx context.Context) error {
-	_, err := getResponse(log, ctx, "старт", comm.T(x), "01WRD,01,0102,0001")
-	return err
+	return getResponse(log, ctx, "старт", comm.T(x), "01WRD,01,0102,0001", nil)
 }
 
 func (x T2500) Stop(log comm.Logger, ctx context.Context) error {
-	_, err := getResponse(log, ctx, "стоп", comm.T(x), "01WRD,01,0102,0004")
-	return err
+	return getResponse(log, ctx, "стоп", comm.T(x), "01WRD,01,0102,0004", nil)
 }
 
 func (x T2500) Setup(log comm.Logger, ctx context.Context, value float64) error {
 	v := int64(value * 10)
 	s := fmt.Sprintf("01WRD,01,0104,%04X", v)
-	_, err := getResponse(log, ctx, fmt.Sprintf("уставка %v⁰C", value), comm.T(x), s)
-	return err
+	return getResponse(log, ctx, fmt.Sprintf("уставка %v⁰C", value), comm.T(x), s, nil)
 }
 
-func (x T2500) Read(log comm.Logger, ctx context.Context) (float64, error) {
-	return getResponse(log, ctx, "запрос температуры", comm.T(x), "01RRD,02,0001,0002")
+func (x T2500) Read(log comm.Logger, ctx context.Context) (temperature float64, err error) {
+	err = getResponse(log, ctx, "запрос температуры", comm.T(x), "01RRD,02,0001,0002", &temperature)
+	return
 }
